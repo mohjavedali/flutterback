890ef06f570d39def47ad83f738d9ab48710e005
Javed Khan (Node Dev) <javed.khan@technosoftwares.com>
2023-01-10 08:26:09 +0530

    first commit

---
+++ controllers/product.controller.js
@@ -0,0 +1,19 @@
+const Product = require('../models/product.model');
+
+//Simple version, without validation or sanitation
+// controllers/products.js
+exports.product_create = function (req, res) {
+    let product = new Product(
+        {
+            name: req.body.name,
+            price: req.body.price
+        }
+    );
+
+    product.save(function (err) {
+        if (err) {
+            return next(err);
+        }
+        res.send('Product Created successfully')
+    })
+};
---
+++ server.js
@@ -0,0 +1,27 @@
+const express = require('express');
+const bodyParser = require('body-parser');
+// initialize our express app
+const app = express();
+const router = express.Router();
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({extended: false}));
+const mongoose = require('mongoose');
+mongoose
+  .connect('mongodb+srv://admin-javed:admin12345@cluster0.kgsa5.mongodb.net/demoProject?retryWrites=true&w=majority')
+  .then(() => {
+    console.log('Connection Successful');
+  })
+  .catch((err) => {
+    console.log(err.message);
+  });
+const product = require('./routes/product.route');
+app.get('/', (req,res)=>{
+    res.send({"message":"first hi value."});
+});
+app.get('/notes', (req, res)=>{
+    res.send("hi");
+});
+let port = process.env.PORT || 5000;
+app.listen(port, () => {
+    console.log('Server is up and running on port numner ' + port);
+});
